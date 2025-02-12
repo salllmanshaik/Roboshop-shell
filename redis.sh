bd8d475f85cda3f5643c6b9c49b28156c37f201a
Salman <hello.salmanshaik@gmail.com>
2025-02-12 16:58:35 +0530

    Frontend

---
+++ redis.sh
@@ -0,0 +1,11 @@
+dnf module disable redis -y
+dnf module enable redis:7 -y
+
+dnf install redis -y
+
+sed -i 's|127.0.0.1|0.0.0.0' /etc/redis/redis.conf
+
+sed 'protected-mode c protected-mode no' /etc/redis/redis.conf
+
+systemctl enable redis
+systemctl start redis
