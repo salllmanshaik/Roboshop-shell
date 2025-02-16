d19823df96622093ae895a315bd91cd68a93d899
Salman <hello.salmanshaik@gmail.com>
2025-02-16 17:32:04 +0530

    user

--- frontend.sh
+++ frontend.sh
@@ -12,10 +12,10 @@ rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
 
 
-# shellcheck disable=SC2164
-cd /usr/share/nginx/html
+
+cd /usr/share/nginx/html || exit
 unzip /tmp/frontend.zip
 
-nginx -t
+
 systemctl enable nginx
 systemctl restart nginx
