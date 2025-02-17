fc768e3d7bcb0d72b56a0694f1100e2f3fbb0b14
Salman <hello.salmanshaik@gmail.com>
2025-02-17 16:39:04 +0530

    user

--- common.sh
+++ common.sh
@@ -1,13 +1,5 @@
 
 
-artifact_download(){
-  rm -rf /app
-  mkdir /app
-  curl -L -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component-v3.zip
-  cd /app
-  unzip /tmp/$component.zip
-}
-
 
 systemd_setup(){
   systemctl daemon-reload
@@ -20,6 +12,15 @@ app_prereq(){
     cp $component.service /etc/systemd/system/$component.service
 }
 
+
+
+artifact_download(){
+  rm -rf /app
+  mkdir /app
+  curl -L -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component-v3.zip
+  cd /app
+  unzip /tmp/$component.zip
+}
 
 nodejs_app_setup(){
   dnf module disable nodejs -y
