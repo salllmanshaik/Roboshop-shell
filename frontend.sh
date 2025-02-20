a90da8c096aa2a8a8d771912c41d110a979752ad
Salman <hello.salmanshaik@gmail.com>
2025-02-20 16:58:36 +0530

    user

--- frontend.sh
+++ frontend.sh
@@ -1,40 +1,40 @@
 source common.sh
 
 print_head Disable Default Nginx
-dnf module disable nginx -y &>> $log_file
+dnf module disable nginx -y &>>$log_file
 exit_status_print $?
 
 print_head Enable Nginx 24
-dnf module enable nginx:1.24 -y &>> $log_file
+dnf module enable nginx:1.24 -y &>>$log_file
 exit_status_print $?
 
 
 print_head Install Nginx
-dnf install nginx -y &>> $log_file
+dnf install nginx -y &>>$log_file
 exit_status_print $?
 
 print_head Copy Nginx configuration file
-cp nginx.conf  /etc/nginx/nginx.conf &>> $log_file
+cp nginx.conf  /etc/nginx/nginx.conf &>>$log_file
 exit_status_print $?
 
 print_head clean old content
-rm -rf /usr/share/nginx/html/* &>> $log_file
+rm -rf /usr/share/nginx/html/* &>>$log_file
 exit_status_print $?
 
 print_head download app content
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>> $log_file
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$log_file
 exit_status_print $?
 
 cd /usr/share/nginx/html
 
 print_head Extract app content
-unzip /tmp/frontend.zip &>> $log_file &>> $log_file
+unzip /tmp/frontend.zip &>> $log_file &>>$log_file
 exit_status_print $?
 
 print_head Enable Nginx service
-systemctl enable nginx &>> $log_file
+systemctl enable nginx &>>$log_file
 exit_status_print $?
 
 print_head Start Nginx service
-systemctl restart nginx &>> $log_file
+systemctl restart nginx &>>$log_file
 exit_status_print $?
